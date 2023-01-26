b854f45f234c0602caad0eefaa60e1597e8325b8
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-01-26 06:22:00 -0500

    fixing a typo

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! [<img alt="github" src="https://img.shields.io/badge/github-aspectron/nw--sys-8da0cb?style=for-the-badge&labelColor=555555&color=8da0cb&logo=github" height="20">](https://github.com/aspectron/nw-sys)
 //! [<img alt="crates.io" src="https://img.shields.io/crates/v/nw-sys.svg?maxAge=2592000&style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/nw-sys)
 //! [<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-nw--sys-56c2a5?maxAge=2592000&style=for-the-badge&logo=rust" height="20">](https://docs.rs/nw-sys)
-//! <img alt="license" src="https://img.shields.io/crates/l/workflow-rpc.svg?maxAge=2592000&color=6ac&style=for-the-badge&logo=opensourceinitiative&logoColor=fff" height="20">
+//! <img alt="license" src="https://img.shields.io/crates/l/nw-sys.svg?maxAge=2592000&color=6ac&style=for-the-badge&logo=opensourceinitiative&logoColor=fff" height="20">
 //!
 //! # Overview
 //!
