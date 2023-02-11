0219af4327be81ff8289fda8bb298899dfb46625
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-02-11 16:21:41 -0500

    update dependencies

--- src/prelude.rs
+++ src/prelude.rs
@@ -8,14 +8,13 @@ pub use crate::result;
 pub use std::sync::{Arc, Mutex};
 
 pub use crate::menu_item::Type as MenuItemType;
-pub use crate::nw::{is_nw, try_nw};
+pub use crate::nw::{is_nw, try_nw, Nw};
 pub use crate::tray::Options as TrayOptions;
 pub use crate::utils::window;
 pub use crate::window;
 pub use crate::Clipboard;
 pub use crate::Menu;
 pub use crate::MenuItem;
-pub use crate::Nw;
 pub use crate::Shortcut;
 pub use crate::Tray;
 pub use crate::Window;
